fc6f93083d0dc690e9eb5736088dbfb28e0cb3ce
github-action-benchmark <github@users.noreply.github.com>
2026-03-05 20:54:39 +0000

    add Replay 10K PR (go) benchmark result for 16df19ef13f66e1aa40efa23715e7a474e22a97a

--- dev/replay-10k/pr-1594/data.js
+++ dev/replay-10k/pr-1594/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1772743781937,
+  "lastUpdate": 1772744079187,
   "repoUrl": "https://github.com/ava-labs/firewood",
   "entries": {
     "Replay 10K PR": [
@@ -130,6 +130,62 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkReplayLog - ns/propose-on-db",
             "value": 125787092,
+            "unit": "ns/propose-on-db",
+            "extra": "3 times\n8 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "ava-labs",
+            "username": "ava-labs"
+          },
+          "committer": {
+            "name": "ava-labs",
+            "username": "ava-labs"
+          },
+          "id": "16df19ef13f66e1aa40efa23715e7a474e22a97a",
+          "message": "feat(replay): performance action (5/5) ",
+          "timestamp": "2026-03-05T19:15:38Z",
+          "url": "https://github.com/ava-labs/firewood/pull/1594/commits/16df19ef13f66e1aa40efa23715e7a474e22a97a"
+        },
+        "date": 1772744078865,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkReplayLog",
+            "value": 158811551,
+            "unit": "ns/op\t      1000 commits\t    387570 ns/\t  32575049 ns/commit\t 124449141 ns/propose-on-db",
+            "extra": "3 times\n8 procs"
+          },
+          {
+            "name": "BenchmarkReplayLog - ns/op",
+            "value": 158811551,
+            "unit": "ns/op",
+            "extra": "3 times\n8 procs"
+          },
+          {
+            "name": "BenchmarkReplayLog - commits",
+            "value": 1000,
+            "unit": "commits",
+            "extra": "3 times\n8 procs"
+          },
+          {
+            "name": "BenchmarkReplayLog - ns/",
+            "value": 387570,
+            "unit": "ns/",
+            "extra": "3 times\n8 procs"
+          },
+          {
+            "name": "BenchmarkReplayLog - ns/commit",
+            "value": 32575049,
+            "unit": "ns/commit",
+            "extra": "3 times\n8 procs"
+          },
+          {
+            "name": "BenchmarkReplayLog - ns/propose-on-db",
+            "value": 124449141,
             "unit": "ns/propose-on-db",
             "extra": "3 times\n8 procs"
           }
