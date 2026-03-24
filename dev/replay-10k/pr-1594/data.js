b750363e2cde3047b4382f835f5e21ad08bdfc3b
github-action-benchmark <github@users.noreply.github.com>
2026-03-24 06:13:08 +0000

    add Replay 10K PR (go) benchmark result for 889d01e35e5211b229cadd507706919cd9e7e9a5

--- dev/replay-10k/pr-1594/data.js
+++ dev/replay-10k/pr-1594/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1774275051646,
+  "lastUpdate": 1774332788455,
   "repoUrl": "https://github.com/ava-labs/firewood",
   "entries": {
     "Replay 10K PR": [
@@ -75,6 +75,44 @@ window.BENCHMARK_DATA = {
             "name": "BenchmarkReplayLog - s/propose-on-db",
             "value": 0.062000077,
             "unit": "s/propose-on-db",
+            "extra": "3 times\n8 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "ava-labs",
+            "username": "ava-labs"
+          },
+          "committer": {
+            "name": "ava-labs",
+            "username": "ava-labs"
+          },
+          "id": "889d01e35e5211b229cadd507706919cd9e7e9a5",
+          "message": "feat(replay): performance action (5/5) ",
+          "timestamp": "2026-03-24T00:03:40Z",
+          "url": "https://github.com/ava-labs/firewood/pull/1594/commits/889d01e35e5211b229cadd507706919cd9e7e9a5"
+        },
+        "date": 1774332788009,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkReplayLog",
+            "value": 106431689,
+            "unit": "ns/op\t      1000 commits",
+            "extra": "3 times\n8 procs"
+          },
+          {
+            "name": "BenchmarkReplayLog - ns/op",
+            "value": 106431689,
+            "unit": "ns/op",
+            "extra": "3 times\n8 procs"
+          },
+          {
+            "name": "BenchmarkReplayLog - commits",
+            "value": 1000,
+            "unit": "commits",
             "extra": "3 times\n8 procs"
           }
         ]
